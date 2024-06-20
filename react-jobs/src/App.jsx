b8adff66967eb2e0a4b1218e4632aa9807436e45
James Lieu <j.lieu888@gmail.com>
2024-06-20 14:56:17 +0100

    feat(react-jobs): Add edit functionality

--- react-jobs/src/App.jsx
+++ react-jobs/src/App.jsx
@@ -10,6 +10,7 @@ import JobsPage from './pages/JobsPage';
 import NotFoundPage from './pages/NotFoundPage';
 import JobPage, { jobLoader } from './pages/JobPage';
 import AddJobPage from './pages/AddJobPage';
+import EditJobPage from './pages/EditJobPage';
 
 const App = () => {
   const addJob = async (newJob) => {
@@ -19,6 +20,18 @@ const App = () => {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify(newJob)
+    });
+
+    return;
+  };
+
+  const editJobSubmit = async (job) => {
+    await fetch(`/api/jobs/${job.id}`, { 
+      method: "PUT", 
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(job)
     });
 
     return;
@@ -39,6 +52,7 @@ const App = () => {
         <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob}/>} />
         <Route path='/jobs' element={<JobsPage />} />
         <Route path='/jobs/:id' element={<JobPage deleteJobSubmit={deleteJobSubmit}/>} loader={jobLoader} />
+        <Route path='/edit-job/:id' element={<EditJobPage editJobSubmit={editJobSubmit} />} loader={jobLoader} />
         <Route path='*' element={<NotFoundPage />} />
       </Route>
     )
